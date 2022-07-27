087342ebbdd3c0854b53616f8368f2818a780eca
Yiren Liu <yirenl2@illinois.edu>
2022-07-26 22:49:17 -0500

    EL with BLINK (ongoing);

---
+++ preparators_EL_BLINK.py
@@ -0,0 +1,84 @@
+"""
+Preparator that performs entity linking on the input text.
+Using Facebook BLINK: https://github.com/facebookresearch/BLINK
+"""
+
+import blink.main_dense as main_dense
+import blink.ner as NER
+import argparse
+
+def _annotate(ner_model, input_sentences):
+    ner_output_data = ner_model.predict(input_sentences)
+    sentences = ner_output_data["sentences"]
+    mentions = ner_output_data["mentions"]
+    samples = []
+    for mention in mentions:
+        record = {}
+        record["label"] = "unknown"
+        record["label_id"] = -1
+        # LOWERCASE EVERYTHING !
+        record["context_left"] = sentences[mention["sent_idx"]][
+            : mention["start_pos"]
+        ].lower()
+        record["context_right"] = sentences[mention["sent_idx"]][
+            mention["end_pos"] :
+        ].lower()
+        record["mention"] = mention["text"].lower()
+        record["start_pos"] = int(mention["start_pos"])
+        record["end_pos"] = int(mention["end_pos"])
+        record["sent_idx"] = mention["sent_idx"]
+        samples.append(record)
+    return samples
+
+
+
+models_path = "models/" # the path where you stored the BLINK models
+
+config = {
+    "test_entities": None,
+    "test_mentions": None,
+    "interactive": False,
+    "top_k": 10,
+    "biencoder_model": models_path+"biencoder_wiki_large.bin",
+    "biencoder_config": models_path+"biencoder_wiki_large.json",
+    "entity_catalogue": models_path+"entity.jsonl",
+    "entity_encoding": models_path+"all_entities_large.t7",
+    "crossencoder_model": models_path+"crossencoder_wiki_large.bin",
+    "crossencoder_config": models_path+"crossencoder_wiki_large.json",
+    "fast": False, # set this to be true if speed is a concern
+    "output_path": "logs/" # logging directory
+}
+
+args = argparse.Namespace(**config)
+
+models = main_dense.load_models(args, logger=None)
+
+
+## TODO: Run NER before BLINK
+text = "test test test"
+
+ner_model = NER.get_model()
+samples = _annotate(ner_model, [text])
+
+## TODO: Convert samples to BLINK input
+
+
+data_to_link = [ {
+                    "id": 0,
+                    "label": "unknown",
+                    "label_id": -1,
+                    "context_left": "".lower(),
+                    "mention": "Shakespeare".lower(),
+                    "context_right": "'s account of the Roman general Julius Caesar's murder by his friend Brutus is a meditation on duty.".lower(),
+                },
+                {
+                    "id": 1,
+                    "label": "unknown",
+                    "label_id": -1,
+                    "context_left": "Shakespeare's account of the Roman general".lower(),
+                    "mention": "Julius Caesar".lower(),
+                    "context_right": "'s murder by his friend Brutus is a meditation on duty.".lower(),
+                }
+                ]
+
+_, _, _, _, _, predictions, scores, = main_dense.run(args, None, *models, test_data=data_to_link)
