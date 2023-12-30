f57ad359442991269f5459cf6a56d19cd2e60111
Lan Li <lilan.scut@gmail.com>
2023-12-30 15:56:00 -0600

    evaluate results..version1

--- dittoPlus/ev_results.py
+++ dittoPlus/ev_results.py
@@ -1,22 +1,44 @@
 import json
 
-with open('result.json', 'r')as f:
+# no knowledge injection 
+with open('./output/Structured/iTunes-Amazon/None/prompt=space/result.json', 'r')as f:
     data = json.load(f)
 
-with open('../prompt=space/result.json', 'r')as f1:
-    data1 = json.load(f1)
+# with knowledge (sherlock)...
+with open('./output/Structured/iTunes-Amazon/sherlock/prompt=space/result.json', 'r')as f1:
+    data_aug = json.load(f1)
 
 
 # same knowledge injection method, different prompting method
 #  data: prompt with slash; data1: prompt with space 
 rows = data['rows']
-rows1 = data1['rows']
-assert len(rows) == len(rows1)
+rows_aug = data_aug['rows']
+assert len(rows) == len(rows_aug)
 
+false_pred = 0
+false_pred_aug = 0
 for i, row in enumerate(rows):
-    row1 = rows1[i]
-    gd = row['ground_truth']
-    pred_res = row['pred_result']
-    pred_res1 = row1['pred_result']
+    row_aug = rows_aug[i]
+    # print(f"original data inputs left: {row['left']}")
+    # print(f"original data inputs right: {row['right']}")
+    gd = int(row['ground_truth'])
+    pred_res = int(row['pred_result'])
+    pred_res_aug = int(row_aug['pred_result'])
     # x, x' --> y, y'
+    if pred_res_aug==gd and pred_res!=gd:
+        print(f'Improved row index: {i}')
+        print(f"Without knowledge injection, predicted result: {row['pred_result']}")
+        print(f"With knowledge injection, predicted result: {row_aug['pred_result']}")
+    if pred_res_aug!=gd:
+        # print(f'wrong predict row with KA: {i}')
+        false_pred_aug += 1 
+        # print(f'False predicted row: {i}')
+        # print(f'Matching ground truth: {gd}')
+    if pred_res!=gd:
+        print(f'wrong predict row without KA: {i}')
+        false_pred += 1
+
+print(f'Total wrong predicted rows without KA: {false_pred}')
+print(f'Total wrong predicted rows with KA: {false_pred_aug}')
+
 
