81f85b6fe1d41afe87e6b9edfc22af7f9ac915c7
Lan Li <lilan.scut@gmail.com>
2024-01-13 23:13:47 -0600

    evaluate script

--- dittoPlus/ev_results.py
+++ dittoPlus/ev_results.py
@@ -1,44 +1,153 @@
 import json
+import pandas as pd
+import os
 
-# no knowledge injection 
-with open('./output/Structured/iTunes-Amazon/None/prompt=space/result.json', 'r')as f:
-    data = json.load(f)
+def read_res(file_path):
+    with open(file_path, 'r')as f:
+        data = json.load(f)
+    return data 
 
-# with knowledge (sherlock)...
-with open('./output/Structured/iTunes-Amazon/sherlock/prompt=space/result.json', 'r')as f1:
-    data_aug = json.load(f1)
-
-
-# same knowledge injection method, different prompting method
-#  data: prompt with slash; data1: prompt with space 
-rows = data['rows']
-rows_aug = data_aug['rows']
-assert len(rows) == len(rows_aug)
-
-false_pred = 0
-false_pred_aug = 0
-for i, row in enumerate(rows):
-    row_aug = rows_aug[i]
-    # print(f"original data inputs left: {row['left']}")
-    # print(f"original data inputs right: {row['right']}")
-    gd = int(row['ground_truth'])
-    pred_res = int(row['pred_result'])
-    pred_res_aug = int(row_aug['pred_result'])
-    # x, x' --> y, y'
-    if pred_res_aug==gd and pred_res!=gd:
-        print(f'Improved row index: {i}')
-        print(f"Without knowledge injection, predicted result: {row['pred_result']}")
-        print(f"With knowledge injection, predicted result: {row_aug['pred_result']}")
-    if pred_res_aug!=gd:
-        # print(f'wrong predict row with KA: {i}')
-        false_pred_aug += 1 
-        # print(f'False predicted row: {i}')
-        # print(f'Matching ground truth: {gd}')
-    if pred_res!=gd:
-        print(f'wrong predict row without KA: {i}')
-        false_pred += 1
-
-print(f'Total wrong predicted rows without KA: {false_pred}')
-print(f'Total wrong predicted rows with KA: {false_pred_aug}')
+def check_distance(detailed_df, idx_same=4, idx_diff=2):
+    # the distance between vectors should represent the similarity between the data entries
+    # predicted_wt_sherlock: T, predicted_wt_doduo: T
+    # predicted_wt_sherlock: F, predicted_wt_doduo: T
+    # cos_(v1, v2)
+    print(detailed_df)
+    predict_same_rows = detailed_df.iloc[idx_same]['Rows Indices']
+    predict_diff_rows = detailed_df.iloc[idx_diff]['Rows Indices']
+    print(predict_same_rows)
+    print(predict_diff_rows)
 
 
+def write_ev_table(data_no_ka, data_sherlock, data_doduo, data_el=None):
+    '''
+    #@params data_no_ka: data without knowledge augmentation
+    #@params data_sherlock: data with column semantic types predicted by sherlock
+    #@params data_doduo: data with column semantic types predicted by doduo
+    #@params data_el: data with entity linking 
+    '''
+    rows_index_details = []
+    values = []
+    rows = data_no_ka['rows']
+    rows_sh = data_sherlock['rows']
+    rows_doduo = data_doduo['rows']
+    assert len(rows) == len(rows_sh) == len(rows_doduo)
+
+    true_all = 0
+    true_all_rows = []
+    false_doduo = 0
+    false_doduo_rows = []
+    false_sh = 0
+    false_sh_rows = []
+    false_both_ka = 0
+    false_both_ka_rows = []
+    false_no_ka = 0
+    false_no_ka_rows = []
+    true_sh = 0
+    true_sh_rows = []
+    true_doduo = 0
+    true_doduo_rows = []
+    false_all = 0
+    false_all_rows = []
+
+    for i, row in enumerate(rows):
+        row_sh = rows_sh[i]
+        row_doduo = rows_doduo[i]
+        gd = int(row['ground_truth'])
+        pred_res = int(row['pred_result'])
+        pred_res_sh = int(row_sh['pred_result'])
+        pred_res_doduo = int(row_doduo['pred_result'])
+        # x, x' --> y, y'
+        if pred_res_sh==gd and pred_res_doduo==gd and pred_res==gd:
+            true_all += 1
+            true_all_rows.append(i)
+        
+        if pred_res==gd and pred_res_sh==gd and pred_res_doduo!=gd:
+            false_doduo += 1
+            false_doduo_rows.append(i)
+
+        if pred_res==gd and pred_res_sh!=gd and pred_res_doduo==gd:
+            false_sh += 1
+            false_sh_rows.append(i)
+        
+        if pred_res==gd and pred_res_sh!=gd and pred_res_doduo!=gd:
+            false_both_ka += 1
+            false_both_ka_rows.append(i)
+
+        if pred_res!=gd and pred_res_sh==gd and pred_res_doduo==gd:
+            false_no_ka += 1
+            false_no_ka_rows.append(i)
+
+        if pred_res!=gd and pred_res_sh==gd and pred_res_doduo!=gd:
+            true_sh += 1
+            true_sh_rows.append(i)
+
+        if pred_res!=gd and pred_res_sh!=gd and pred_res_doduo==gd:
+            true_doduo += 1
+            true_doduo_rows.append(i)
+
+        if pred_res!=gd and pred_res_sh!=gd and pred_res_doduo!=gd: 
+            false_all += 1
+            false_all_rows.append(i) 
+    true_all_ratio = true_all/len(rows)
+    values.append(['T', 'T', 'T', true_all, true_all_ratio])
+    rows_index_details.append(['Predicted results are all correct: with or without KA', true_all_rows])
+    false_doduo_ratio = false_doduo/len(rows)
+    values.append(['T', 'T', 'F', false_doduo, false_doduo_ratio])
+    rows_index_details.append(['Knowledge augmented by Doduo worse the predicted results', false_doduo_rows])
+    false_sh_ratio = false_sh/len(rows)
+    values.append(['T', 'F', 'T', false_sh, false_sh_ratio])
+    rows_index_details.append(['Knowledge augmented by Sherlock worse the predicted results', false_sh_rows])
+    false_both_ka_ratio = false_both_ka/len(rows)
+    values.append(['T', 'F', 'F', false_both_ka, false_both_ka_ratio])
+    rows_index_details.append(['Knowledge augmented by both methods worse the predicted results', false_both_ka_rows])
+    false_no_ka_ratio = false_no_ka/len(rows)
+    values.append(['F', 'T', 'T', false_no_ka, false_no_ka_ratio])
+    rows_index_details.append(['Knowledge augmented by both methods improve the predicted results', false_no_ka_rows])
+    true_sh_ratio = true_sh/len(rows)
+    values.append(['F', 'T', 'F', true_sh, true_sh_ratio])
+    rows_index_details.append(['Knowledge augmented by Sherlock improve the predicted results', true_sh_rows])
+    true_doduo_ratio = true_doduo/len(rows)
+    values.append(['F', 'F', 'T', true_doduo, true_doduo_ratio])
+    rows_index_details.append(['Knowledge augmented by Duoduo improve the predicted results', true_doduo_rows])
+    false_all_ratio = false_all/len(rows)
+    values.append(['F', 'F', 'F', false_all, false_all_ratio])
+    rows_index_details.append(['Predicted results are all False: with or without KA', false_all_rows])
+    # print(values)
+    return values, rows_index_details
+
+
+def main():
+    column_names = ['Predicted result[Without KA]', 'Predicted Result[With Sherlock]', 'Predicted Result[With Doduo]', 'Rows Count', 'Error Ratio']
+    col_names_bp = ['Description', 'Rows Indices']
+    ev_df = pd.DataFrame(columns=column_names)
+    details_df = pd.DataFrame(columns=col_names_bp)
+    data_no_ka = read_res('./output/Structured/DBLP-ACM/None/prompt=space/result.json')
+    data_doduo = read_res('./output/Structured/DBLP-ACM/doduo/prompt=space/result.json')
+    data_sherlock = read_res('./output/Structured/DBLP-ACM/sherlock/prompt=space/result.json')
+
+    rows_list, details_list = write_ev_table(data_no_ka, data_sherlock, data_doduo)
+    for rows in rows_list:
+        ev_df = ev_df.append(pd.Series(rows, index=column_names), ignore_index=True)
+    
+    for details in details_list:
+        details_df = details_df.append(pd.Series(details, index=col_names_bp), ignore_index=True)
+    
+    analysis_fp = 'output_analysis.csv'
+    details_fp = 'output_details.csv'
+    if os.path.exists(analysis_fp):
+        print(f'The file path {analysis_fp} exists.')
+    else:
+
+        ev_df.to_csv(analysis_fp, index=False)
+        details_df.to_csv(details_fp, index=False)
+        # print(f'The file path {analysis_fp} does not exist.')
+    
+    print('done')
+    check_distance(details_df)
+
+
+
+
+if __name__ == '__main__':
+    main()
